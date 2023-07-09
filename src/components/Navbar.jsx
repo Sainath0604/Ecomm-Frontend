cc71c22cf405bae6b42b4c2263cd2151fe5063fc
Sainath Patil <pspatilsai64@gmail.com>
2023-07-09 23:34:46 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,10 +26,10 @@ function Navbar() {
               <div className="relative h-[10vh] flex items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 dark:hover:bg-[#D9CFFC] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 dark:text-[#241B35] hover:bg-gray-700 dark:hover:bg-[#D9CFFC] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
-                      <div className="block h-6 w-6">
+                      <div className="block h-6 w-6 ">
                         <CrossIcon />
                       </div>
                     ) : (
@@ -121,8 +121,8 @@ function Navbar() {
                     <Disclosure.Button
                       className={classNames(
                         item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          ? "bg-gray-900 text-[#241B35]"
+                          : "text-gray-300 dark:text-[#241B35] hover:bg-gray-700 dark:hover:bg-[#D9CFFC] hover:text-white",
                         "block rounded-md px-3 py-2 text-base font-medium"
                       )}
                     >
@@ -132,6 +132,9 @@ function Navbar() {
                 ))}
                 <div className="sm:hidden">
                   <Searchbar />
+                </div>
+                <div className="sm:hidden">
+                  <Theme />
                 </div>
               </div>
             </Disclosure.Panel>
