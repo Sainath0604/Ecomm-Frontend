3a20c12f00a6691580aeaf95b6211a362babd898
Sainath Patil <pspatilsai64@gmail.com>
2023-07-14 02:00:47 +0530

    Added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import StateProvider from "./toolkit/StateProvider";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <StateProvider>
       <App />
+      <Analytics />
     </StateProvider>
   </React.StrictMode>
 );
